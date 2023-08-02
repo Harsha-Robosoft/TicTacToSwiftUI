f20efc3de2004d86df7534cba4b01ad4db1cf8c6
harsha <harsh.mundaragi@robosoftin.com>
2023-08-02 15:06:15 +0530

    Initial Commit

---
+++ TicTacToSwiftUI/TicTacToSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToSwiftUIApp.swift
+//  TicTacToSwiftUI
+//
+//  Created by Harsha R Mundaragi  on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
